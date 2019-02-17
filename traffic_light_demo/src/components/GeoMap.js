bef3bf98f1f2655c4fcecfdf267c04ee3704b1aa
Karel D'haene <karel.dhaene@gmail.com>
2019-02-17 14:44:57 +0100

    update dependencies, remove unused imports

--- traffic_light_demo/src/components/GeoMap.js
+++ traffic_light_demo/src/components/GeoMap.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import {Map, TileLayer, Marker, Popup} from 'react-leaflet';
+import {Map, TileLayer} from 'react-leaflet';
 
 export default class GeoMap extends React.Component {
     constructor(){
         super();
         this.lat = 51.21205;
-        this.lon = 4.39717;
+        this.lng = 4.39717;
         this.zoom = 19;
     }
 
     render(){
-        const position = [this.lat, this.lon];
+        const position = [this.lat, this.lng];
         return (
             <Map style={{width:'600px', height: '600px'}} center={position} zoom={this.zoom}>
                 <TileLayer
