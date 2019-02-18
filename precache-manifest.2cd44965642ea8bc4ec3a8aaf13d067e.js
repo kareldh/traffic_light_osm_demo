12ddd9ddb9d1638310beebb9104a6d13bdee487a
kareldh <karel.dhaene@gmail.com>
2019-02-18 14:34:44 +0100

    Updates

---
+++ precache-manifest.2cd44965642ea8bc4ec3a8aaf13d067e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ade1c3bc3f2aaca2b9b",
+    "url": "/kareldh/traffic_light_osm_demo/static/js/runtime~main.8ade1c3b.js"
+  },
+  {
+    "revision": "9b13d966e2bd8740ce22",
+    "url": "/kareldh/traffic_light_osm_demo/static/js/main.9b13d966.chunk.js"
+  },
+  {
+    "revision": "2f5e437dcde7d7c450f3",
+    "url": "/kareldh/traffic_light_osm_demo/static/js/2.2f5e437d.chunk.js"
+  },
+  {
+    "revision": "9b13d966e2bd8740ce22",
+    "url": "/kareldh/traffic_light_osm_demo/static/css/main.fa6f9310.chunk.css"
+  },
+  {
+    "revision": "2f5e437dcde7d7c450f3",
+    "url": "/kareldh/traffic_light_osm_demo/static/css/2.2a75ea67.chunk.css"
+  },
+  {
+    "revision": "da861564d428363560d0431b320b263e",
+    "url": "/kareldh/traffic_light_osm_demo/index.html"
+  }
+];
